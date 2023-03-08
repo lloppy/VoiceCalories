35e214f7313a30dce5c8215633937f3702b97293
lloppy <ankudinovapol@gmail.com>
2023-03-08 21:42:02 +0500

    add data and data's models

---
+++ app/src/main/java/com/example/voicecalories/data/model/Ingredient.kt
@@ -0,0 +1,14 @@
+package com.example.voicecalories.data.model
+
+data class Ingredient(
+    val id: Int,
+    val image: String,
+    val name: String,
+    val amount: Double,
+    val unit: String
+) {
+    val imageUrl: String
+        get() {
+            return "https://spoonacular.com/cdn/ingredients_100x100/$image"
+        }
+}
---
+++ app/src/main/java/com/example/voicecalories/data/model/Recipe.kt
@@ -0,0 +1,20 @@
+package com.example.voicecalories.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Recipe(
+    @SerializedName("spoonacularScore")
+    val score: Double,
+    val pricePerServing: Double,
+    val id: Int,
+    val title: String,
+    val readyInMinutes: Int,
+    val servings: Int,
+    val image: String,
+    val summary: String,
+    val instructions: String,
+    @SerializedName("spoonacularSourceUrl")
+    val url: String,
+    @SerializedName("extendedIngredients")
+    val ingredients: List<Ingredient>
+)
