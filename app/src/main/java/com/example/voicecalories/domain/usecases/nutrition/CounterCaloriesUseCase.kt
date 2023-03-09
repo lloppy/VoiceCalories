ca45b7b3f874eac3696e61c53faa6f6ecc854354
lloppy <ankudinovapol@gmail.com>
2023-03-09 23:45:23 +0500

    add caloriesSum in CounterCaloriesUseCase

--- app/src/main/java/com/example/voicecalories/domain/usecases/nutrition/CounterCaloriesUseCase.kt
+++ app/src/main/java/com/example/voicecalories/domain/usecases/nutrition/CounterCaloriesUseCase.kt
@@ -1,4 +1,9 @@
 package com.example.voicecalories.domain.usecases.nutrition
 
 class CounterCaloriesUseCase {
+    private var calories = 1000
+
+    fun caloriesSum(): Int {
+        return calories
+    }
 }
